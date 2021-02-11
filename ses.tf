70e4d9a9de02dc96c029d7d2b0342e6779f35b57
Karthikeyanngithub <karthi1232003@gmail.com>
2021-02-12 00:41:21 +0530

    added ses

---
+++ ses.tf
@@ -0,0 +1,54 @@
+resource "aws_ses_email_identity" "sender" {
+  email = var.sender_mail
+}
+
+resource "aws_iam_user" "sender" {
+  name = element(split("@", var.sender_mail), 0)
+  path = "/SESSMTP/"
+}
+
+resource "aws_iam_access_key" "sender" {
+  user = aws_iam_user.sender.name
+}
+
+//output "aws_iam_smtp_password_v4_support" {
+//  value = aws_iam_access_key.sender.ses_smtp_password
+//}
+//output "aws_iam_smtp_key" {
+//  value = aws_iam_access_key.sender.id
+//}
+//
+//output "aws_iam_smtp_user" {
+//  value = aws_iam_access_key.sender.user
+//}
+
+
+resource "aws_iam_policy" "ses_policy_support" {
+  name        = "ses_policy"
+  path        = "/SESSMTP/"
+  description = "policy to use ses to sent emails"
+
+  policy = <<EOF
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Effect":"Allow",
+      "Action":[
+        "ses:SendEmail",
+        "ses:SendRawEmail"
+      ],
+      "Resource":"*"
+    }
+  ]
+}
+EOF
+}
+
+
+
+resource "aws_iam_policy_attachment" "ses_policy-attach" {
+  name       = "ses_policy-attach"
+  users      = ["${aws_iam_user.support.name}"]
+  policy_arn = aws_iam_policy.ses_policy_support.arn
+}
